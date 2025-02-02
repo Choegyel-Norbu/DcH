0e2992d0a5c665cbb0789afddd36f11c645ffe85
Choegyel <mac@Chogyal.local>
2025-02-02 08:25:17 +0600

    first commit

---
+++ src/navigations/stack/screens/MainDashBoradScreen.jsx
@@ -0,0 +1,9 @@
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+export default function MainDashBoradScreen() {
+  return <SafeAreaView style={{flex: 1}}></SafeAreaView>;
+}
+
+const styles = StyleSheet.create({});
